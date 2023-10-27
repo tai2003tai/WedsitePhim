d8da6324bfdd5df9fabd802dc33637fcfe71c109
tai2003tai <nguyentai290290@gmail.com>
2023-10-27 19:34:15 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WedsitePhim.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Phim()
+        {
+            return View();
+        }
+        public IActionResult ThanhVien()
+        {
+            return View();
+        }
+        public IActionResult tramchieuphim()
+        {
+            return View();
+        }
+        public IActionResult tatcacactram()
+        {
+            return View();
+        }
+
+        public IActionResult KhuyenMai  ()
+        {
+            return View();
+        }
+        public IActionResult Cachdatcho()
+        {
+            return View();
+        }
+        public IActionResult Cachdatchocuoicung()
+        {
+            return View();
+        }
+        public IActionResult Cachdatchomua()
+        {
+            return View();
+        }
+        public IActionResult Cachdatchobuoc1()
+        {
+            return View();
+        }
+        public IActionResult Cachdatchobuoc2()
+        {
+            return View();
+        }
+        public IActionResult Cachdatchomuatruoc()
+        {
+            return View();
+        }
+        public IActionResult ketnoi()
+        {
+            return View();
+        }
+    }
+}
